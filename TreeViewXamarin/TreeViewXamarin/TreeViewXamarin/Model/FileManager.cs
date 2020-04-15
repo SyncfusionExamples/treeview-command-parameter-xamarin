3c788f1d4b2df845f79ff1c34601e7ebad9099bf
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-04-15 12:47:09 +0530

    XAMARIN-34268 - Sample added.

---
+++ TreeViewXamarin/TreeViewXamarin/TreeViewXamarin/Model/FileManager.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+using Xamarin.Forms;
+
+namespace TreeViewXamarin
+{
+    public class FileManager : INotifyPropertyChanged
+    {
+        private bool isVisible = true;
+        private string fileName;
+        private ImageSource imageIcon;
+        private ObservableCollection<FileManager> subFiles;
+
+        public FileManager()
+        {
+
+        }
+        public ObservableCollection<FileManager> SubFiles
+        {
+            get { return subFiles; }
+            set
+            {
+                subFiles = value;
+                RaisedOnPropertyChanged("SubFiles");
+            }
+        }
+
+        public string ItemName
+        {
+            get { return fileName; }
+            set
+            {
+                fileName = value;
+                RaisedOnPropertyChanged("ItemName");
+            }
+        }
+        public bool ISVisible
+        {
+            get { return isVisible; }
+            set
+            {
+                isVisible = value;
+                RaisedOnPropertyChanged("ISVisible");
+            }
+        }
+        public ImageSource ImageIcon
+        {
+            get { return imageIcon; }
+            set
+            {
+                imageIcon = value;
+                RaisedOnPropertyChanged("ImageIcon");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void RaisedOnPropertyChanged(string _PropertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(_PropertyName));
+            }
+        }
+    }
+}
